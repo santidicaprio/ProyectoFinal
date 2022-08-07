35a2bfbb137f2cedda0d5f57178ed0bac375b4ad
Santiago Garcia <garciasantiagogabriel777@gmail.com>
2022-08-07 12:43:20 -0300

    Limpieza

--- ProyectoFinal/AppFinal/views.py
+++ ProyectoFinal/AppFinal/views.py
@@ -92,24 +92,7 @@ class ProductoDetailView(DetailView):
 ## Hasta aca modificacion 
 
 
-def saludo(request):
-	return render(request, "AppFinal/proyecto.html")
 
-
-def formulario(request):
-    if request.method== 'POST':
-        formu = MiFormulario(request.POST)
-        print(formu)
-
-
-    if formu.is_valid:
-        informacion = MiFormulario.cleaned_data
-        
-        persona = MiFormulario(nombre=informacion['nombre'], apellido=informacion['apellido',])
-        persona.save()
-
-def index(req):
-	return render(req, 'AppFinal/index.html')
 
 
     
